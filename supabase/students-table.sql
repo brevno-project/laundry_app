9261b9b2a14735cf644a62c0391426201560fa2d
Artem <sway_dikon@mail.ru>
2025-10-28 15:48:48 +0600

    Update students list with real 34 students from Block A and B

--- supabase/students-table.sql
+++ supabase/students-table.sql
@@ -23,10 +23,42 @@ CREATE POLICY "Allow all insert" ON public.students FOR INSERT WITH CHECK (true)
 -- Включить Realtime
 ALTER PUBLICATION supabase_realtime ADD TABLE public.students;
 
--- Добавить примерных студентов (замените на своих)
+-- Добавить студентов
 INSERT INTO public.students ("firstName", "lastName", "fullName", room, "isRegistered") VALUES
-  ('Артем', 'Андреевич', 'Артем Андреевич', 'A501', false),
-  ('Иван', 'Петров', 'Иван Петров', 'A502', false),
-  ('Мария', 'Сидорова', 'Мария Сидорова', 'A503', false),
-  ('Дмитрий', 'Козлов', 'Дмитрий Козлов', 'A504', false),
-  ('Анна', 'Смирнова', 'Анна Смирнова', 'A505', false);
+  -- Block B
+  ('Aigiza', '', 'Aigiza', 'B201', false),
+  ('Lim Haneul', '', 'Lim Haneul', 'B201', false),
+  ('Aijan', '', 'Aijan', 'B301', false),
+  ('Aimira', '', 'Aimira', 'B301', false),
+  ('Aizada', '', 'Aizada', 'B301', false),
+  ('Sezim', 'Koichumanova', 'Sezim Koichumanova', 'B301', false),
+  ('Janara', '', 'Janara', 'B302', false),
+  ('Sezim', 'Kabylbekova', 'Sezim Kabylbekova', 'B302', false),
+  ('Asylgul', '', 'Asylgul', 'B302', false),
+  ('Uultai', '', 'Uultai', 'B302', false),
+  
+  -- Block A
+  ('Iman', '', 'Iman', 'A201', false),
+  ('Aziret', '', 'Aziret', 'A201', false),
+  ('Nurtilek', '', 'Nurtilek', 'A201', false),
+  ('Nurislam', 'Abdykasymov', 'Nurislam Abdykasymov', 'A201', false),
+  ('Nurel', '', 'Nurel', 'A201', false),
+  ('Semyon', '', 'Semyon', 'A202', false),
+  ('Baihan', '', 'Baihan', 'A202', false),
+  ('Nurislam', 'Abdyldaev', 'Nurislam Abdyldaev', 'A202', false),
+  ('Elnatan', '', 'Elnatan', 'A301', false),
+  ('David', '', 'David', 'A301', false),
+  ('Aktan', '', 'Aktan', 'A301', false),
+  ('Nurtilek', 'Orozbekov', 'Nurtilek Orozbekov', 'A301', false),
+  ('Bektur', '', 'Bektur', 'A301', false),
+  ('Sunnat', '', 'Sunnat', 'A301', false),
+  ('Tamerlan', '', 'Tamerlan', 'A302', false),
+  ('Joomart', '', 'Joomart', 'A302', false),
+  ('Erjan', '', 'Erjan', 'A302', false),
+  ('Aibek', '', 'Aibek', 'A302', false),
+  ('Artem', '', 'Artem', 'A501', false),
+  ('Pavel', '', 'Pavel', 'A501', false),
+  ('Kylym', '', 'Kylym', 'A402', false),
+  ('Azim', '', 'Azim', 'A402', false),
+  ('Akbiy', '', 'Akbiy', 'A402', false),
+  ('Bayastan', '', 'Bayastan', 'A402', false);
